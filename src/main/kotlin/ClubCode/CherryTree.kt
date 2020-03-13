be1a26e1942007fe1732320e7968abbc25632547
Alexandre Conte <alexandre.conte@telecom-sudparis.eu>
2020-03-14 00:46:53 +0100

    Un meilleur cherry

--- src/main/kotlin/ClubCode/CherryTree.kt
+++ src/main/kotlin/ClubCode/CherryTree.kt
@@ -5,9 +5,9 @@ import processing.core.PConstants
 import processing.core.PVector
 
 
-const val MOTION_BLUR_ALPHA_FACTOR = 30f
+const val MOTION_BLUR_ALPHA_FACTOR = 150f
 const val STOP_LENGTH = 25.0f
-const val OSCILLATION_AMPLITUDE_MAX = 6.0f
+const val OSCILLATION_AMPLITUDE_MAX = 3.0f
 const val FREQ_WEIGHT_COMPRESSION = 1f
 
 const val FREQ_MIN = 0.02f
@@ -22,8 +22,13 @@ const val TRUNC_WIDTH_DECREASE_MAX = 0.9f
 const val TRUNC_LENGTH_DECREASE_MIN = 0.8f
 const val TRUNC_LENGTH_DECREASE_MAX = 0.9f
 
-const val INIT_ORIENTATION_DIFF_MIN = 20f
-const val INIT_ORIENTATION_DIFF_MAX = 40f
+const val TRUNC_STEP_LENGTH_MIN = 10f
+const val TRUNC_STEP_LENGTH_MAX = 20f
+const val TRUNC_STEP_ORIENTATION_DIFF_MIN = -15f
+const val TRUNC_STEP_ORIENTATION_DIFF_MAX = 15f
+
+const val INIT_ORIENTATION_DIFF_MIN = -20f
+const val INIT_ORIENTATION_DIFF_MAX = 20f
 
 const val ORIENTATION_DECREASE_MIN = 0.8f
 const val ORIENTATION_DECREASE_MAX = 1.2f
@@ -35,14 +40,14 @@ class Program : PApplet() {
     lateinit var root: Tree
 
     inner class Tree(private val truncLength: Float, private val truncWidth: Float, private val orientationDifference: Float, orientationMax: Float){
-        private val phase: Float = random(PHASE_MIN, PHASE_MAX)
-        private var oscillationAmplitude = random(radians(OSCILLATION_AMPLITUDE_MAX))
-        private val frequency = random(FREQ_MIN, FREQ_MAX) / log(1 + this.truncWidth * FREQ_WEIGHT_COMPRESSION)
         private var left: Tree? = null
         private var right: Tree? = null
+        private val trunc = Trunc(truncLength, truncWidth, orientationDifference)
+
 
         init {
             if(truncLength > STOP_LENGTH) {
+
                 val noSplitting = random(1f) <= NO_SPLITTING_PROBA
                 val coinToss = random(1f) <= 0.5f
 
@@ -67,19 +72,15 @@ class Program : PApplet() {
         }
 
         fun noOscillation() {
-            this.oscillationAmplitude = 0f
+            this.trunc.noOscillation()
         }
 
         fun draw(t: Int, position: PVector, orientation: Float){
-            strokeWeight(truncWidth)
-            val end = PVector.add(position, PVector
-                .fromAngle(orientation + oscillationAmplitude * sin(frequency*t + phase))
-                .mult(truncLength))
-            line(position.x, position.y, end.x, end.y)
+            val (end, newOrientation) = trunc.draw(t, position, orientation)
 
-            if(truncLength > STOP_LENGTH) {
-                left?.let { it.draw(t, end, orientation + it.orientationDifference) }
-                right?.let { it.draw(t, end, orientation + it.orientationDifference) }
+            if (truncLength > STOP_LENGTH) {
+                left?.draw(t, end, newOrientation)
+                right?.draw(t, end, newOrientation)
             } else {
                 strokeWeight(0f)
                 fill(250f, 80f, 120f, 120f)
@@ -87,6 +88,55 @@ class Program : PApplet() {
             }
         }
     }
+
+
+    inner class Trunc(val length: Float, val width: Float, val orientationDifference: Float){
+        private val truncSteps: MutableList<TruncStep> = mutableListOf()
+        private val phase: Float = random(PHASE_MIN, PHASE_MAX)
+        private var oscillationAmplitude = random(radians(OSCILLATION_AMPLITUDE_MAX))
+        private val frequency = random(FREQ_MIN, FREQ_MAX) / log(1 + width * FREQ_WEIGHT_COMPRESSION)
+
+        init {
+            var sum = 0f
+
+            while (sum < length){
+                val l = random(TRUNC_STEP_LENGTH_MIN, TRUNC_STEP_LENGTH_MAX)
+                truncSteps.add(TruncStep(l, width, radians(random(TRUNC_STEP_ORIENTATION_DIFF_MIN, TRUNC_STEP_ORIENTATION_DIFF_MAX))))
+                sum += l
+            }
+        }
+
+        fun noOscillation(){
+            oscillationAmplitude = 0f
+        }
+
+        fun draw(t: Int, position: PVector, orientation: Float): Pair<PVector, Float>{
+            var end: PVector = position
+            var stepOrientation = orientation + orientationDifference + oscillationAmplitude * sin(frequency*t + phase)
+
+            for(step in truncSteps){
+                val pair = step.draw(end, stepOrientation)
+                end = pair.first
+                stepOrientation = pair.second
+            }
+
+            return end to stepOrientation
+        }
+
+    }
+
+    inner class TruncStep(val length: Float, val width:Float, val orientationDifference: Float){
+        fun draw(position: PVector, orientation: Float): Pair<PVector, Float>{
+            val newOrientation = orientation + orientationDifference
+            val end = PVector.add(position, PVector
+                .fromAngle(newOrientation)
+                .mult(length))
+            strokeWeight(width)
+            line(position.x, position.y, end.x, end.y)
+            return end to newOrientation
+        }
+    }
+
 
     override fun settings() {
         size(1500, 800)
@@ -99,12 +149,12 @@ class Program : PApplet() {
         background(230f, 250f, 220f)
         stroke(80f, 0f, 50f, 200f)
         ellipseMode(CENTER)
-
+//        Trunc(100f,20f, radians(random(INIT_ORIENTATION_DIFF_MIN, INIT_ORIENTATION_DIFF_MAX))).draw(0, PVector(width.toFloat() / 2, height.toFloat()), radians(-90f))
         this.mouseClicked()
     }
 
     override fun mouseClicked() {
-        root = Tree(random(100f, 120f), 10f, radians(random(INIT_ORIENTATION_DIFF_MIN, INIT_ORIENTATION_DIFF_MAX)), radians(45f))
+        root = Tree(random(100f, 120f), 30f, radians(random(INIT_ORIENTATION_DIFF_MIN, INIT_ORIENTATION_DIFF_MAX)), radians(45f))
         root.noOscillation()
     }
 
