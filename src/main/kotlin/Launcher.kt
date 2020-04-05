6484969bb19e4c7b8b81d4c350f747f25b44b671
Paul Mabileau <paulmabileau@hotmail.fr>
2020-04-06 01:27:42 +0200

    Added falling particles to launcher
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -5,6 +5,8 @@ import pi.main as piMain
 import randomsierpinski.main as randomsierpinskiMain
 import genetic.main as geneticMain
 import textgenerator.main as textgeneratorMain
+import fallingparticles.main as fallingparticlesMain
+
 
 fun main(args: Array<String>) {
     val projects = listOf(
@@ -14,18 +16,22 @@ fun main(args: Array<String>) {
         "SquarePi" to ::piMain,
         "RandomSierpinski" to ::randomsierpinskiMain,
         "Rocket" to ::geneticMain,
-        "TextGenerator" to ::textgeneratorMain
+        "TextGenerator" to ::textgeneratorMain,
+        "FallingParticles" to ::fallingparticlesMain
     )
     var found = false
-    while (!found) {
 
+    while (!found) {
         for ((index, value) in projects.withIndex()) {
             println("${value.first}: ${index + 1}")
         }
+
         val a = readLine()
+
         if (a != null) {
             found = true
             val x = a.toInt()
+
             if (x - 1 in projects.indices) {
                 projects[x - 1].second.invoke(args)
             }
