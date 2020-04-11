5de90f666acf182c8c8cdf121f43e67f618eca4f
Alexandre Conte <alexandre.conte@telecom-sudparis.eu>
2020-04-11 23:02:41 +0200

    Ok tier mondrian

--- src/main/kotlin/mondrian/Mondrian.kt
+++ src/main/kotlin/mondrian/Mondrian.kt
@@ -2,11 +2,12 @@ package mondrian
 
 import processing.core.PApplet
 import processing.core.PVector
+import kotlin.math.pow
 import kotlin.random.Random
 
 const val MIN_LENGTH = 100f
-const val RANDOM_DIRECTION = 1f / 3
-const val RANDOM_CONTINUE = 1f / 2
+const val RANDOM_DIRECTION = 0.33
+const val RANDOM_CONTINUE = 0.75
 
 class Program : PApplet() {
 
@@ -18,7 +19,7 @@ class Program : PApplet() {
                 this@Program.width.toFloat()*this@Program.height.toFloat(), 0f, 1f)
             val direction = Random.nextFloat()
             if (width >= 2 * MIN_LENGTH && height >= 2 * MIN_LENGTH) {
-                if(Random.nextFloat() < size*2) {
+                if(Random.nextFloat() < size.pow(0.2f)) {
                     if(direction < 1f / 2) {
                         val position = Random.nextFloat() * (width - 2 * MIN_LENGTH) + MIN_LENGTH
                         val left = Rectangle(x, y, position, height)
@@ -37,7 +38,7 @@ class Program : PApplet() {
                 }
 
             } else if (width >= 2 * MIN_LENGTH) {
-                if (direction < RANDOM_CONTINUE) {
+                if (direction < RANDOM_CONTINUE || width > height * 4) {
                     val position = Random.nextFloat() * (width - 2 * MIN_LENGTH) + MIN_LENGTH
                     val left = Rectangle(x, y, position, height)
                     val right = Rectangle(x + position, y, width- position, height)
@@ -47,7 +48,7 @@ class Program : PApplet() {
                     draw()
                 }
             } else if (height >= 2 * MIN_LENGTH) {
-                if (direction < RANDOM_CONTINUE) {
+                if (direction < RANDOM_CONTINUE || height > width * 4) {
                     val position = Random.nextFloat() * (height - 2 * MIN_LENGTH) + MIN_LENGTH
                     val down = Rectangle(x, y, width, position)
                     val up = Rectangle(x, y + position, width, height - position)
@@ -76,8 +77,10 @@ class Program : PApplet() {
 
     override fun setup() {
         colors = listOf(
-            PVector(255f, 0f, 0f), PVector(255f, 255f, 255f), PVector(255f, 255f, 0f),
-            PVector(0f, 0f, 255f)
+            PVector(255f, 0f, 0f), PVector(255f, 255f, 0f),
+            PVector(0f, 0f, 255f),
+            PVector(255f, 255f, 255f), PVector(255f, 255f, 255f), PVector(255f, 255f, 255f),
+            PVector(0f, 0f, 0f)
         )
         mouseClicked()
     }
