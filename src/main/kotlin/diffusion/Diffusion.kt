3655bf1ab82f4f9f50f3275e79788255e4d6b3f5
Alexandre Conte <alexandre.conte@telecom-sudparis.eu>
2020-04-14 14:22:02 +0200

    Diffusion-Réaction de Gray-Scott

---
+++ src/main/kotlin/diffusion/Diffusion.kt
@@ -0,0 +1,148 @@
+package diffusion
+
+import processing.core.PApplet
+
+const val GRID_WIDTH = 300
+const val GRID_HEIGHT = 160
+
+const val CELL_SIZE = 5
+
+const val DIFFUSION_RATE_A = 1f
+const val DIFFUSION_RATE_B = 0.5f
+const val FEED = 0.055f
+const val KILL = 0.062f
+
+const val DELTA_TIME = 1f
+
+
+class Program : PApplet() {
+
+    private var grid = Grid(GRID_WIDTH, GRID_HEIGHT)
+    private var next = Grid(GRID_WIDTH, GRID_HEIGHT)
+
+    inner class Grid(val width: Int, val height: Int) {
+
+        private val grid = Array(GRID_HEIGHT) { Array(GRID_WIDTH) { Chemicals(1f, 0f) } }
+
+        fun laplacian(i: Int, j: Int): Pair<Float, Float> {
+            var resA = 0f
+            resA += this.getA(i, j) * -1f
+            resA += this.getA(i, j - 1) * 0.2f
+            resA += this.getA(i, j + 1) * 0.2f
+            resA += this.getA(i - 1, j) * 0.2f
+            resA += this.getA(i + 1, j) * 0.2f
+
+            resA += this.getA(i - 1, j - 1) * 0.05f
+            resA += this.getA(i - 1, j + 1) * 0.05f
+            resA += this.getA(i + 1, j + 1) * 0.05f
+            resA += this.getA(i + 1, j - 1) * 0.05f
+
+            var resB = 0f
+            resB += this.getB(i, j) * -1f
+            resB += this.getB(i, j - 1) * 0.2f
+            resB += this.getB(i, j + 1) * 0.2f
+            resB += this.getB(i - 1, j) * 0.2f
+            resB += this.getB(i + 1, j) * 0.2f
+
+            resB += this.getB(i - 1, j - 1) * 0.05f
+            resB += this.getB(i - 1, j + 1) * 0.05f
+            resB += this.getB(i + 1, j + 1) * 0.05f
+            resB += this.getB(i + 1, j - 1) * 0.05f
+            return Pair(resA, resB)
+        }
+
+        fun getCell(i: Int, j: Int): Chemicals {
+            return if (j in 0 until height && i in 0 until width) {
+                grid[j][i]
+            } else {
+                throw ArrayIndexOutOfBoundsException("attention")
+            }
+        }
+
+        fun getA(i: Int, j: Int): Float {
+            return if (j in 0 until height && i in 0 until width) {
+                grid[j][i].A
+            } else {
+                1f
+            }
+        }
+
+        fun setA(i: Int, j: Int, a: Float) {
+            if (j in 0 until height && i in 0 until width) {
+                grid[j][i].A = a
+            }
+        }
+
+        fun getB(i: Int, j: Int): Float {
+            return if (j in 0 until height && i in 0 until width) {
+                grid[j][i].B
+            } else {
+                0f
+            }
+        }
+
+        fun setB(i: Int, j: Int, b: Float) {
+            if (j in 0 until height && i in 0 until width) {
+                grid[j][i].B = b
+            }
+        }
+    }
+
+    inner class Chemicals(var A: Float, var B: Float) {
+        fun show(x: Int, y: Int) {
+            val colorA = color(220, 220, 220)
+            val colorB = color(0, 0, 0)
+            val colorCell = lerpColor(colorA, colorB, B / (A + B))
+            fill(colorCell)
+            rect(x.toFloat(), y.toFloat(), CELL_SIZE.toFloat(), CELL_SIZE.toFloat())
+        }
+    }
+
+    override fun settings() {
+        size(1500, 800)
+    }
+
+    override fun setup() {
+        noStroke()
+        val RECT_W = 10
+        val RECT_H = 10
+        for (i in 0 until RECT_W) {
+            for (j in 0 until RECT_H) {
+                grid.setA(GRID_WIDTH / 2 + i - RECT_W / 2, GRID_HEIGHT / 2 + j - RECT_H / 2, 0f)
+                grid.setB(GRID_WIDTH / 2 + i - RECT_W / 2, GRID_HEIGHT / 2 + j - RECT_H / 2, 1f)
+            }
+        }
+    }
+
+    override fun draw() {
+        for (i in 0 until GRID_WIDTH) {
+            for (j in 0 until GRID_HEIGHT) {
+                val A = grid.getA(i, j)
+                val B = grid.getB(i, j)
+                val L = grid.laplacian(i, j)
+                next.setA(i, j, A + (DIFFUSION_RATE_A * L.first - A * B * B + FEED * (1 - A)) * DELTA_TIME)
+                next.setB(i, j, B + (DIFFUSION_RATE_B * L.second + A * B * B - (KILL + FEED) * B) * DELTA_TIME)
+            }
+        }
+        val temp = grid
+        grid = next
+        next = temp
+
+        for (i in 0 until GRID_WIDTH) {
+            for (j in 0 until GRID_HEIGHT) {
+                val cell = grid.getCell(i, j)
+                cell.show(i * CELL_SIZE, j * CELL_SIZE)
+            }
+        }
+
+    }
+
+    fun run() {
+        runSketch()
+    }
+}
+
+
+fun main(args: Array<String>) {
+    Program().run()
+}
